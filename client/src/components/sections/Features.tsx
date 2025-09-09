4be056b56372146354c9ff407d75dfb0ed8ed605
SntgRM <santy01052007@gmail.com>
2025-09-09 14:31:03 -0500

    REF - change in the main language of the section

--- client/src/components/sections/Features.tsx
+++ client/src/components/sections/Features.tsx
@@ -9,7 +9,7 @@ export const Features = () => {
       {" "}
       <Container className="space-y-10 md:space-y-12">
         <div className="text-center max-w-3xl mx-auto space-y-4">
-          <Title>Características</Title>
+          <Title>Features</Title>
         </div>
 
         <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8">
