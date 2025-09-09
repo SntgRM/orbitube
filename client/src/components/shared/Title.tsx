d4cc48660dc2193af92d9f492fd6c0ad67bb7036
SntgRM <santy01052007@gmail.com>
2025-09-09 14:26:33 -0500

    REF - change in the title component to prevent the navbar from overlapping the title of each section

--- client/src/components/shared/Title.tsx
+++ client/src/components/shared/Title.tsx
@@ -4,7 +4,7 @@ interface TitleProps {
 
 export const Title = ({ children }: TitleProps) => {
   return (
-    <h1 className="text-heading-1 font-semibold text-2xl sm:text-3xl md:text-4xl">
+    <h1 className="text-heading-1 mt-30 font-semibold text-3xl sm:text-3xl md:text-4xl">
       {" "}
       {children}{" "}
     </h1>
