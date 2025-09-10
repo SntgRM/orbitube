dafc707c35035e89725d3249d01c4dc1b887c317
SntgRM <santy01052007@gmail.com>
2025-09-10 13:54:08 -0500

    ADD - CORS added to handle allowed origins

--- server/app/main.py
+++ server/app/main.py
@@ -1,15 +1,23 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from app.routes import converter
 
 app = FastAPI(
-    title="Orbitube API",
+    title="OrbiTube API",
     description="Convertidor de YouTube a MP3/MP4",
     version="1.0.0",
 )
 
-# Incluir rutas
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(converter.router)
 
 @app.get("/")
 def root():
-    return {"message": "API funcionando correctamente 🚀"}
+    return {"message": "OK"}
