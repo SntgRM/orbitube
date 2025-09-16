52887236c868055547473317bfb99f81d72d72c2
SntgRM <santy01052007@gmail.com>
2025-09-16 10:54:16 -0500

    ADD - add optional onClick prop to NavItem component

--- client/src/components/shared/NavItem.tsx
+++ client/src/components/shared/NavItem.tsx
@@ -1,12 +1,14 @@
 interface NavItemProps {
   href: string
   text: string
+  onClick?: (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void
 }
 
-export const NavItem = ({ href, text }: NavItemProps) => {
+export const NavItem = ({ href, text, onClick }: NavItemProps) => {
   return (
     <a
       href={href}
+      onClick={onClick}
       className="relative px-4 py-2 rounded-lg text-sm font-medium transition-all duration-200 hover:text-white hover:bg-gray-800/50"
     >
       {text}
