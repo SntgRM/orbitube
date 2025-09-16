654c6d7d6cc8c53b6cb23eea6be0bf8fd53538b1
SntgRM <santy01052007@gmail.com>
2025-09-16 15:43:32 -0500

    ADD - added translation hook to render the dynamic title and obtain feature data through the hook to maintain consistency with the i18n implementation. Added bottom margin to improve spacing

--- client/src/components/sections/Features.tsx
+++ client/src/components/sections/Features.tsx
@@ -1,15 +1,18 @@
-import { features } from "../../utils/features-data";
-import { Feature } from "../cards/FeatureCard";
-import { Container } from "../shared/Container";
-import { Title } from "../shared/Title";
+import { useFeaturesData } from "../../utils/features-data"
+import { Feature } from "../cards/FeatureCard"
+import { Container } from "../shared/Container"
+import { Title } from "../shared/Title"
+import { useTranslation } from "react-i18next"
 
 export const Features = () => {
+  const { t } = useTranslation()
+  const features = useFeaturesData()
+
   return (
-    <section id="features">
-      {" "}
+    <section className="mb-20" id="features">
       <Container className="space-y-10 md:space-y-12">
         <div className="text-center max-w-3xl mx-auto space-y-4">
-          <Title>Features</Title>
+          <Title>{t("features.sectionTitle")}</Title>
         </div>
 
         <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8">
@@ -24,5 +27,5 @@ export const Features = () => {
         </div>
       </Container>
     </section>
-  );
-};
+  )
+}
